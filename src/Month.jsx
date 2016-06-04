b8235945151a72894d3f769e3b36d382b79a8c9c
Gregory Beaver <greg@chiaraquartet.net>
2016-06-04 10:31:39 -0500

    refactor to es6 class

--- src/Month.jsx
+++ src/Month.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { findDOMNode } from 'react-dom';
 import cn from 'classnames';
 import dates from './utils/dates';
@@ -29,57 +29,78 @@ let eventsForWeek = (evts, start, end, props) =>
 
 let isSegmentInSlot = (seg, slot) => seg.left <= slot && seg.right >= slot;
 
-let propTypes = {
-  ...EventRow.PropTypes,
-
-  culture: React.PropTypes.string,
-
-  date: React.PropTypes.instanceOf(Date),
-
-  min: React.PropTypes.instanceOf(Date),
-  max: React.PropTypes.instanceOf(Date),
-
-  dateFormat,
-
-  weekdayFormat: dateFormat,
-
-  popup: React.PropTypes.bool,
-
-  popupOffset: React.PropTypes.oneOfType([
-    React.PropTypes.number,
-    React.PropTypes.shape({
-      x: React.PropTypes.number,
-      y: React.PropTypes.number
-    })
-  ]),
-
-  onSelectEvent: React.PropTypes.func,
-  onSelectSlot: React.PropTypes.func
-};
+let propTypes;
 
 
-let MonthView = React.createClass({
+export default class Month extends Component {
+  static displayName = 'MonthView'
 
-  displayName: 'MonthView',
+  static propTypes = {
+    ...EventRow.PropTypes,
 
-  propTypes,
+    culture: React.PropTypes.string,
 
-  getInitialState(){
-    return {
+    date: React.PropTypes.instanceOf(Date),
+
+    min: React.PropTypes.instanceOf(Date),
+    max: React.PropTypes.instanceOf(Date),
+
+    dateFormat,
+
+    weekdayFormat: dateFormat,
+
+    popup: React.PropTypes.bool,
+
+    popupOffset: React.PropTypes.oneOfType([
+      React.PropTypes.number,
+      React.PropTypes.shape({
+        x: React.PropTypes.number,
+        y: React.PropTypes.number
+      })
+    ]),
+
+    onSelectEvent: React.PropTypes.func,
+    onSelectSlot: React.PropTypes.func
+  }
+
+  static navigate(date, action) {
+    switch (action){
+      case navigate.PREVIOUS:
+        return dates.add(date, -1, 'month');
+
+      case navigate.NEXT:
+        return dates.add(date, 1, 'month')
+
+      default:
+        return date;
+    }
+  }
+
+  static range(date, { culture }) {
+    let start = dates.firstVisibleDay(date, culture)
+    let end = dates.lastVisibleDay(date, culture)
+    return { start, end }
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
       rowLimit: 5,
       needLimitMeasure: true
     }
-  },
+    this._renderMeasureRows = this._renderMeasureRows.bind(this)
+    this._selectEvent = this._selectEvent.bind(this)
+  }
 
   componentWillMount() {
     this._bgRows = []
-  },
+  }
 
   componentWillReceiveProps({ date }) {
     this.setState({
       needLimitMeasure: !dates.eq(date, this.props.date)
     })
-  },
+  }
 
   componentDidMount() {
     let running;
@@ -95,46 +116,135 @@ let MonthView = React.createClass({
         })
       }
     }, false)
-  },
+  }
 
   componentDidUpdate(prevProps, prevState) {
     if (this.state.needLimitMeasure)
       this._measureRowLimit(this.props)
-  },
+  }
 
   componentWillUnmount() {
     window.removeEventListener('resize', this._resizeListener, false)
-  },
+  }
 
-  render() {
-    var { date, culture, weekdayFormat, onSelectSlot } = this.props
-      , month = dates.visibleDays(date, culture)
-      , weeks  = chunk(month, 7);
+  _measureRowLimit(props) {
+    let eventHeight = getHeight(this._measureEvent);
+    let labelHeight = getHeight(this._firstDateRow);
+    let eventSpace = getHeight(this._firstRow) - labelHeight;
 
-    let measure = this.state.needLimitMeasure
+    this._needLimitMeasure = false;
 
-    this._weekCount = weeks.length;
+    this.setState({
+      needLimitMeasure: false,
+      rowLimit: Math.max(
+        Math.floor(eventSpace / eventHeight), 1)
+    })
+  }
 
-    var elementProps = omit(this.props, Object.keys(propTypes));
-    
-    return (
-      <SelectableMonthView
-        {...elementProps}
-        selectable={this.props.selectable}
-        constantSelect
-        selectIntermediates
-        onFinishSelect={(...args) => onSelectSlot(args[2])}
-        className={cn('rbc-month-view', elementProps.className)}
-      >
-        <div className='rbc-row rbc-month-header'>
-          {this._headers(weeks[0], weekdayFormat, culture)}
+  _dateClick(date, e){
+    e.preventDefault();
+    notify(this.props.onNavigate, [navigate.DATE, date])
+  }
+
+  _selectEvent(...args){
+
+    notify(this.props.onSelectEvent, args)
+  }
+
+  _showMore(segments, date, weekIdx, slot){
+    let cell = findDOMNode(this._bgRows[weekIdx]).children[slot - 1];
+
+    let events = segments
+      .filter(seg => isSegmentInSlot(seg, slot))
+      .map(seg => seg.event)
+
+    if (this.props.popup) {
+      let position = getPosition(cell, findDOMNode(this));
+
+      this.setState({
+        overlay: { date, events, position }
+      })
+    }
+    else {
+      notify(this.props.onNavigate, [navigate.DATE, date])
+    }
+
+    notify(this.props.onShowMore, [events, date, slot])
+  }
+
+  _dates(row){
+    return row.map((day, colIdx) => {
+      var offRange = dates.month(day) !== dates.month(this.props.date);
+
+      return (
+        <div
+          key={'header_' + colIdx}
+          style={segStyle(1, 7)}
+          className={cn('rbc-date-cell', {
+            'rbc-off-range': offRange,
+            'rbc-now': dates.eq(day, new Date(), 'day'),
+            'rbc-current': dates.eq(day, this.props.date, 'day')
+          })}
+        >
+          <a href='#' onClick={this._dateClick.bind(null, day)}>
+            { localizer.format(day, this.props.dateFormat, this.props.culture) }
+          </a>
         </div>
-        { weeks.map((week, idx) =>
-            this.renderWeek(week, idx, measure && this._renderMeasureRows))
-        }
-      </SelectableMonthView>
+      )
+    })
+  }
+
+  _headers(row, format, culture){
+    let first = row[0]
+    let last = row[row.length - 1]
+
+    return dates.range(first, last, 'day').map((day, idx) =>
+      <div
+        key={'header_' + idx}
+        className='rbc-header'
+        style={segStyle(1, 7)}
+      >
+        { localizer.format(day, format, culture) }
+      </div>
     )
-  },
+  }
+
+  _renderMeasureRows(levels, row, idx) {
+    let first = idx === 0;
+
+    return first ? (
+      <div className='rbc-row'>
+        <div className='rbc-row-segment' style={segStyle(1, 7)}>
+          <div ref={r => this._measureEvent = r} className={cn('rbc-event')}>
+            <div className='rbc-event-content'>&nbsp;</div>
+          </div>
+        </div>
+      </div>
+    ) : <span/>
+  }
+
+  _renderOverlay(){
+    let overlay = (this.state && this.state.overlay) || {};
+
+    return (
+      <Overlay
+        rootClose
+        placement='bottom'
+        container={this}
+        show={!!overlay.position}
+        onHide={() => this.setState({ overlay: null })}
+      >
+        <Popup
+          {...this.props}
+          position={overlay.position}
+          events={overlay.events}
+          slotStart={overlay.date}
+          slotEnd={overlay.end}
+          onSelect={this._selectEvent}
+        />
+      </Overlay>
+    )
+  }
 
   renderWeek(week, weekIdx, content) {
     let { first, last } = endOfRange(week);
@@ -179,7 +289,7 @@ let MonthView = React.createClass({
         }
       </div>
     )
-  },
+  }
 
   renderBackground(row, idx){
 
@@ -191,14 +301,15 @@ let MonthView = React.createClass({
       getValueFromSlot={slot => row[slot]}
     />
     )
-  },
+  }
 
   renderRowLevel(segments, week, idx){
-    let { first, last } = endOfRange(week);
+    const { first, last } = endOfRange(week);
+    const { selected, ...props } = this.props
 
     return (
       <EventRow
-        {...this.props}
+        {...props}
         eventComponent={this.props.components.event}
         onSelect={this._selectEvent}
         key={idx}
@@ -208,7 +319,7 @@ let MonthView = React.createClass({
         end={last}
       />
     )
-  },
+  }
 
   renderShowMore(segments, extraSegments, week, weekIdx) {
     let { first, last } = endOfRange(week);
@@ -227,145 +338,35 @@ let MonthView = React.createClass({
         end={last}
       />
     )
-  },
+  }
 
-  _dates(row){
-    return row.map((day, colIdx) => {
-      var offRange = dates.month(day) !== dates.month(this.props.date);
+  render() {
+    var { date, culture, weekdayFormat, onSelectSlot } = this.props
+      , month = dates.visibleDays(date, culture)
+      , weeks  = chunk(month, 7);
 
-      return (
-        <div
-          key={'header_' + colIdx}
-          style={segStyle(1, 7)}
-          className={cn('rbc-date-cell', {
-            'rbc-off-range': offRange,
-            'rbc-now': dates.eq(day, new Date(), 'day'),
-            'rbc-current': dates.eq(day, this.props.date, 'day')
-          })}
-        >
-          <a href='#' onClick={this._dateClick.bind(null, day)}>
-            { localizer.format(day, this.props.dateFormat, this.props.culture) }
-          </a>
-        </div>
-      )
-    })
-  },
+    let measure = this.state.needLimitMeasure
 
-  _headers(row, format, culture){
-    let first = row[0]
-    let last = row[row.length - 1]
+    this._weekCount = weeks.length;
 
-    return dates.range(first, last, 'day').map((day, idx) =>
-      <div
-        key={'header_' + idx}
-        className='rbc-header'
-        style={segStyle(1, 7)}
-      >
-        { localizer.format(day, format, culture) }
-      </div>
-    )
-  },
-
-  _renderMeasureRows(levels, row, idx) {
-    let first = idx === 0;
-
-    return first ? (
-      <div className='rbc-row'>
-        <div className='rbc-row-segment' style={segStyle(1, 7)}>
-          <div ref={r => this._measureEvent = r} className={cn('rbc-event')}>
-            <div className='rbc-event-content'>&nbsp;</div>
-          </div>
-        </div>
-      </div>
-    ) : <span/>
-  },
-
-  _renderOverlay(){
-    let overlay = (this.state && this.state.overlay) || {};
+    var elementProps = omit(this.props, Object.keys(Month.propTypes));
 
     return (
-      <Overlay
-        rootClose
-        placement='bottom'
-        container={this}
-        show={!!overlay.position}
-        onHide={() => this.setState({ overlay: null })}
+      <SelectableMonthView
+        {...elementProps}
+        selectable={this.props.selectable}
+        constantSelect
+        selectIntermediates
+        onFinishSelect={(...args) => onSelectSlot(args[2])}
+        className={cn('rbc-month-view', elementProps.className)}
       >
-        <Popup
-          {...this.props}
-          position={overlay.position}
-          events={overlay.events}
-          slotStart={overlay.date}
-          slotEnd={overlay.end}
-          onSelect={this._selectEvent}
-        />
-      </Overlay>
+        <div className='rbc-row rbc-month-header'>
+          {this._headers(weeks[0], weekdayFormat, culture)}
+        </div>
+        { weeks.map((week, idx) =>
+          this.renderWeek(week, idx, measure && this._renderMeasureRows))
+        }
+      </SelectableMonthView>
     )
-  },
-
-  _measureRowLimit(props) {
-    let eventHeight = getHeight(this._measureEvent);
-    let labelHeight = getHeight(this._firstDateRow);
-    let eventSpace = getHeight(this._firstRow) - labelHeight;
-
-    this._needLimitMeasure = false;
-
-    this.setState({
-      needLimitMeasure: false,
-      rowLimit: Math.max(
-        Math.floor(eventSpace / eventHeight), 1)
-    })
-  },
-
-  _dateClick(date, e){
-    e.preventDefault();
-    notify(this.props.onNavigate, [navigate.DATE, date])
-  },
-
-  _selectEvent(...args){
-
-    notify(this.props.onSelectEvent, args)
-  },
-
-  _showMore(segments, date, weekIdx, slot){
-    let cell = findDOMNode(this._bgRows[weekIdx]).children[slot - 1];
-
-    let events = segments
-      .filter(seg => isSegmentInSlot(seg, slot))
-      .map(seg => seg.event)
-
-    if (this.props.popup) {
-      let position = getPosition(cell, findDOMNode(this));
-
-      this.setState({
-        overlay: { date, events, position }
-      })
-    }
-    else {
-      notify(this.props.onNavigate, [navigate.DATE, date])
-    }
-
-    notify(this.props.onShowMore, [events, date, slot])
-  },
-});
-
-MonthView.navigate = (date, action)=>{
-  switch (action){
-    case navigate.PREVIOUS:
-      return dates.add(date, -1, 'month');
-
-    case navigate.NEXT:
-      return dates.add(date, 1, 'month')
-
-    default:
-      return date;
   }
 }
-
-MonthView.range = (date, { culture }) => {
-  let start = dates.firstVisibleDay(date, culture)
-  let end = dates.lastVisibleDay(date, culture)
-  return { start, end }
-}
-
-export default MonthView
